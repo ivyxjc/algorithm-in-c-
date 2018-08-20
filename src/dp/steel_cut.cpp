7f89b27b0172f6bd401513613ecb4a71fe0fd75f
ivyxjc <ivyxjc1994@hotmail.com>
2018-08-20 22:45:55 +0800

    :sparkles: steel cut(bottom up cut rod)

--- src/dp/steel_cut.cpp
+++ src/dp/steel_cut.cpp
@@ -2,14 +2,13 @@
 // Created by ivyxjc on 8/19/2018.
 //
 #include <iostream>
-#include "string.h"
 
 using namespace std;
 
 
 int no_cache_count = 0;
 int cache_count = 0;
-
+int bottom_up_count = 0;
 
 /**
  * 不带备忘的自顶向下的方法
@@ -55,12 +54,45 @@ int cut_rod(int *p, int n, int *value) {
     return q;
 }
 
+/**
+ * 自底而上的方法
+ * @param p
+ * @param n
+ * @return
+ */
 int bottom_up_cut_rod(int *p, int n) {
-    if(n==0){
-        return 0;
+    int *value = new int[n + 1];
+    value[0] = 0;
+
+    for (int j = 1; j <= n; j++) {
+        int q = INT32_MIN;
+        for (int i = 1; i <= j; i++) {
+            bottom_up_count++;
+            q = max(q, p[i] + value[j - i]);
+        }
+        value[j] = q;
     }
+    return value[n];
 }
 
+int *extended_bottom_up_cut_rod(int *p, int n) {
+    int *value = new int[n + 1];
+    int *detail = new int[n + 1];
+    value[0] = 0;
+
+    for (int j = 0; j <= n; j++) {
+        int q = INT32_MIN;
+        for (int i = 0; i <= j; i++) {
+            if (q < p[i] + value[j - i]) {
+                q = p[i] + value[j - i];
+                detail[j] = i;
+            }
+        }
+        value[j] = q;
+    }
+    detail[0] = value[n];
+    return detail;
+}
 
 int main() {
     int *arr = new int[11];
@@ -75,7 +107,7 @@ int main() {
     arr[9] = 24;
     arr[10] = 30;
 
-    int length = 100;
+    int length = 10000;
 
     int *value = new int[length + 1];
     for (int i = 0; i < length + 1; i++) {
@@ -83,9 +115,16 @@ int main() {
     }
 //    int res=cut_rod(arr,length);
     int res2 = cut_rod(arr, length, value);
+    int res3 = bottom_up_cut_rod(arr, length);
+    int res4 = extended_bottom_up_cut_rod(arr, length)[0];
 //    cout<<res<<endl;
+    cout << "=============" << endl;
     cout << res2 << endl;
+    cout << res3 << endl;
+    cout << res4 << endl;
+    cout << "============" << endl;
     cout << no_cache_count << endl;
     cout << cache_count << endl;
+    cout << bottom_up_count << endl;
 }
 
